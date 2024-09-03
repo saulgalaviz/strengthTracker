c3873a541d8c6c2834ec60a1dd52b6ac1bf093fd
saulgalaviz <saulgalaviz97@gmail.com>
2024-09-03 11:30:37 -0700

    Tweak visual to get a sense of visuals.

--- st-frontend/src/components/VisualComponent.jsx
+++ st-frontend/src/components/VisualComponent.jsx
@@ -1,25 +1,44 @@
 import React from 'react';
-import Highcharts from 'highcharts';
+// import Highcharts from 'highcharts';
+import Highcharts from 'highcharts/highstock';
 import HighchartsReact from 'highcharts-react-official';
 
 const VisualComponent = () => {
   const options = {
     chart: {
-      type: 'spline'
+      type: "line"
+    },
+    xAxis: {
+      title: {text: "Date"},
+      categories: ["Dev", "Feb"]
+    },
+    yAxis: {
+      title: {text: "Weight"},
+      categories: ["Fat", "Feb", "Fat", "Feb"]
     },
     title: {
-      text: 'My chart'
+      text: "Bench Press"
     },
     series: [
       {
-        data: [1, 2, 1, 4, 3, 6]
+        data: [1, 2, 1, 4, 3, 6],
+        name: "Bench Press"
+      },
+      {
+        data: [0, 1, 1, 3, 5, 7],
+        name: "Body Weight"
       }
     ]
   };
 
   return (
     <div>
-      <HighchartsReact highcharts={Highcharts} options={options} />
+      <HighchartsReact
+      highcharts={Highcharts} 
+      // constructorType={'stockChart'}
+      options={options} 
+      
+      />
     </div>
   )
 }

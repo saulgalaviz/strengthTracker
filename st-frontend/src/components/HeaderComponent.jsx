deac24fccdb52d0c66088188a100d040e68af9f1
saulgalaviz <saulgalaviz97@gmail.com>
2024-11-01 16:36:40 -0700

    Deployed Docker Containers!

--- st-frontend/src/components/HeaderComponent.jsx
+++ st-frontend/src/components/HeaderComponent.jsx
@@ -7,9 +7,12 @@ const HeaderComponent = () => {
                 <div className="container">
                     <nav className="navbar navbar-dark">
                         <span>
-                        <a className="navbar-brand " href="http://localhost:3000/visual">Visual</a>
+                        {/* <a className="navbar-brand " href="http://localhost:3000/visual">Visual</a>
                         <a className="navbar-brand" href="http://localhost:3000/">Main List</a>
-                        <a className="navbar-brand" href="http://localhost:3000/about">About</a>
+                        <a className="navbar-brand" href="http://localhost:3000/about">About</a> */}
+                        <a className="navbar-brand " href="https://www.strength-tracker.com/visual">Visual</a>
+                        <a className="navbar-brand" href="https://www.strength-tracker.com/">Main List</a>
+                        <a className="navbar-brand" href="https://www.strength-tracker.com/about">About</a>
                         </span>
                     </nav>
                 </div>
--- st-frontend/src/services/LiftService.js
+++ st-frontend/src/services/LiftService.js
@@ -1,6 +1,7 @@
 import axios from "axios"; //Axios is 3rd part library that allows us to make http connection for front end
 
-const REST_API_BASE_URL = "http://localhost:8080/api/lifts"; //points to backend API
+// const REST_API_BASE_URL = "http://localhost:8080/api/lifts"; //points to backend API
+const REST_API_BASE_URL = "https://api.strength-tracker.com/api/lifts";
 
 //REST client code to get all lifts from the REST API
 export const listLifts = () => {
